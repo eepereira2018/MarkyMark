5b9b91f9c5abb552956460016bb3be804b1ce54a
eepereira2018 <ernestepereira@gmail.com>
2022-02-14 21:43:06 -0500

    commit on main

---
+++ app/models/product.rb
@@ -0,0 +1,14 @@
+class Product < ApplicationRecord
+    validates :title, presence: true
+    validates :cost, presence: true
+    validates :image, presence: true
+    validates :description, presence: true, length: { minimum: 10 }
+
+    VALID_STATUSES = ['public', 'private', 'archived']
+
+    validates :status, inclusion: { in: VALID_STATUSES }
+  
+    def archived?
+      status == 'archived'
+    end
+end
