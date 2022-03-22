d54145428a9d2a657ae885b2ba14259bbb3f2f02
eepereira2018 <ernestepereira@gmail.com>
2022-03-22 01:25:28 -0400

    commit on main

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
     validates :title, presence: true
     validates :cost, presence: true
     validates :image, presence: true
+    validates :stock, presence: true
     validates :description, presence: true, length: { minimum: 10 }
 
     VALID_STATUSES = ['public', 'private', 'archived']
